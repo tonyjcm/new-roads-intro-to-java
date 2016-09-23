0c5c71b7f75c6bbcbf10362288e574bd40f04b7c
amerriweather <amerriweather@newroads.org>
2016-09-23 11:54:44 -0400

    loops

---
+++ TestLoops.java
@@ -0,0 +1,17 @@
+public class TestLoops{
+
+public static void main(String[] args){
+  
+  int x = 2;
+  
+  for (int index = 0; index < 10; index++){
+    
+System.out.println(x*index);
+    
+  }
+  
+  
+  }
+
+
+}
