2d4997d648a356dc2afaab9439b8ca84e702ae9a
amerriweather <amerriweather@newroads.org>
2017-01-13 12:32:37 -0500

    cards

--- TestCard.java
+++ TestCard.java
@@ -2,10 +2,28 @@ public class TestCard{
   
   public static void main(String[] args){
     
-    
     Card c1 = new Card();
-    
     System.out.println(c1);
+    
+    c1.setRank("J");
+    System.out.println(c1);
+    
+    
+    Card c2 = new Card();
+    System.out.println(c2);
+    
+      c2.setColor("Q");
+    System.out.println(c2);
+    
+    Card c3 = new Card();
+    System.out.println(c3);
+    
+    
+      c3.setSuit("K");
+    System.out.println(c3);
+    
+    
+   
     
     
   }
